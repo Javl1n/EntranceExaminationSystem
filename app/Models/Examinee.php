c6f72048cdffd58d6b1738dd6deae39b268bd42a
Javl1n <farmodia@gmail.com>
2024-03-08 22:22:00 +0800

    Initial Commit

---
+++ app/Models/Examinee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Examinee extends Model
+{
+    use HasFactory;
+}
